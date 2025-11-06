f7f483d3a641bf8affd1a6a24a1bd9fb5a06807e
dal-b <daliabashaev@gmail.com>
2025-11-05 21:30:41 -0600

    added events to calendar, they are clickable but lead to nothing since a page hasnt been added

--- Calendar/calendar.js
+++ Calendar/calendar.js
@@ -1,7 +1,22 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const calendarEl = document.getElementById("calendar");
+document.addEventListener('DOMContentLoaded', function () {
+  const calendarEl = document.getElementById('calendar');
+
   const calendar = new FullCalendar.Calendar(calendarEl, {
-    initialView: "dayGridMonth",
+    initialView: 'dayGridMonth',
+    events: [
+      {
+        title: 'Monday Night Hired',
+        start: '2025-11-10',
+        url: 'event.html'   // 👈 link to another page
+      },
+      {
+        title: 'Monday Night Hired',
+        start: '2025-11-14',
+        end: '2025-11-16',
+        url: 'event.html' // 👈 another link
+      }
+    ]
   });
+
   calendar.render();
 });
