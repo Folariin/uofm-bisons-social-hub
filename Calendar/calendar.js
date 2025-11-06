f351367d35ca1218a10a0dda640ddcc1a0e5997b
dal-b <daliabashaev@gmail.com>
2025-11-05 21:59:53 -0600

    changes to the stuff in the file, some organization, and fixing links between pages

--- Calendar/calendar.js
+++ Calendar/calendar.js
@@ -1,22 +1,25 @@
+/*
+This part of the code makes the calendar.
+You can add an event using the same format as in the events array
+*/
 document.addEventListener('DOMContentLoaded', function () {
   const calendarEl = document.getElementById('calendar');
-
   const calendar = new FullCalendar.Calendar(calendarEl, {
     initialView: 'dayGridMonth',
     events: [
       {
         title: 'Monday Night Hired',
         start: '2025-11-10',
-        url: 'event.html'   // 👈 link to another page
+        url: '../Event/event.html'
       },
       {
         title: 'Monday Night Hired',
-        start: '2025-11-14',
-        end: '2025-11-16',
-        url: 'event.html' // 👈 another link
+        start: '2025-11-17',
+        end: '2025-11-17',
+        url: '../Event/event.html'
       }
-    ]
+    ],
+    height: '100%',
   });
-
   calendar.render();
 });
